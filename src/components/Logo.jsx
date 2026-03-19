ed6a3420115dffac2adaf1b7654d9a739019114c
aymhki <agamyahk@myumanitoba.ca>
2026-03-19 08:50:57 -0500

    Fixed logo bug

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 function Logo() {
     return (
         <img alt="Veridion logo"
-             src={'/Logo.png'}
+             src={`${import.meta.env.BASE_URL}Logo.png`}
              style={{ width: '32px', height: '32px', borderRadius: '8px', objectFit: 'cover' }}
         />
     )
